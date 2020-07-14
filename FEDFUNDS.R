f5858e48473ab2c68b2ca62fff304557066d66aa
GaspardLeMarque <gleb.ushakov@yahoo.com>
2020-07-14 22:45:28 +0200

    Added Taylor Rule check section

--- FEDFUNDS.R
+++ FEDFUNDS.R
@@ -40,3 +40,43 @@ data %>%
   autoplot(vars(FEDFUNDS, INFLATION, OutputGap)) +
   geom_line(col = "#0c2ac1") +
   xlab("Years")
+
+#---------------------------------------------------------------------------------
+
+#Run linear regression to check the Taylor rule assumption
+fit <- dynlm(FEDFUNDS ~ INFLATION + OutputGap, data = data)
+summary(fit)
+#the linear trend coefficients are significant at the 0.1% significance level
+#When inflation increases by 1% the rate increases by 0.99%
+#When Output Gap increases by 1% the rate increases by 0.55%
+#The results are consistent with the Taylor rule
+
+#Predicted rate based on linear model
+predFEDFUNDS <- predict.lm(fit)
+#Adding NA's to make the vector compatible with the main data frame
+c(rep(NA, length(data$FEDFUNDS) - length(predFEDFUNDS)), predFEDFUNDS)
+length(predFEDFUNDS) <- 271
+
+#Taylor rule rate
+trFEDFUNDS <- 1 + 1.5*data$INFLATION + 0.5*data$OutputGap
+
+#Add 2 new TS to the data frame
+data <- cbind(data, Predicted = predFEDFUNDS, Taylor = trFEDFUNDS)
+
+#Make an interest rates plot to compare 3 types of rates 
+data %>%
+  ggplot() +
+  geom_line(aes(Date, FEDFUNDS, color = "Federal funds rate"), size = 0.5) +
+  geom_line(aes(Date, Predicted, color = "Predicted rate"), size = 0.5) +
+  geom_line(aes(Date, Taylor, color = "Taylor rate"), size = 0.5) +
+  xlab("Years") + ylab("%") +
+  ggtitle("Interest Rates") +
+  scale_colour_manual("", 
+                      values = c("#00FF00", "#FF0000", "#0000FF")) +
+  theme(legend.position = c(.95, .98),
+        legend.justification = c("right", "top"),
+        plot.title = element_text(hjust = 0.5))
+#According to the Taylor Rule:
+#In 2004-2005 period the interest rate should have been higher.
+#In 2009-2010 period the rate should have been lower, but not significantly.
+#In 2012-2015 period the rate should have been significantly higher.
